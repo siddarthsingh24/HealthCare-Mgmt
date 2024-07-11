66c5c4a3916a264ced24cf728c2052ce6cf12b78
Siddarth Singh <96046931+siddarthsingh24@users.noreply.github.com>
2024-07-12 03:51:01 +0530

    Update urls.py

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,6 +1,8 @@
 # main urls.py
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('users.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
