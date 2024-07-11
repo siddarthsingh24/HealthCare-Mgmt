782ac011f720d27b3a87a29bbd64233ca9908528
Siddarth Singh <96046931+siddarthsingh24@users.noreply.github.com>
2024-07-12 03:50:20 +0530

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+from. import views
 
 urlpatterns = [
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('doctor_dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
-    path('patient_dashboard/', views.patient_dashboard, name='patient_dashboard'),
-    path('', views.home, name='home'),  # Add this line
+    path('signup/', views.signup_view, name='signup'),  
+    path('login/', views.login_view, name='login'),  
+    path('accounts/login/', views.login_view, name='login_accounts'),  
+    path('doctor_dashboard/', views.doctor_dashboard, name='doctor_dashboard'), 
+    path('patient_dashboard/', views.patient_dashboard, name='patient_dashboard'), 
+    path('', views.home, name='home'),  
+    path('create_blog_post/', views.create_blog_post, name='create_blog_post'), 
 ]
